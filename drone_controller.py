303fd203f37947f65d0e65df548d4d484b317061
Fynn <fynn.van-westen@gmx.de>
2020-08-06 12:52:30 +0200

    added face search heuristic and currently working beta

--- drone_controller.py
+++ drone_controller.py
@@ -19,6 +19,10 @@ t = take off
 3 = speed lvl. 3 | distance lvl.3
 4 = speed lvl. 4 | distance lvl.4
 5 = speed lvl. 5 | distance lvl.5
+6 = speed lvl. 6 | distance lvl.6
+7 = speed lvl. 7 | distance lvl.7
+8 = speed lvl. 8 | distance lvl.8
+9 = speed lvl. 9 | distance lvl.9
 o = reduce speed by 5
 p = inhance speed by 5
 g = lock oncurrent detection
@@ -35,7 +39,7 @@ class droneController:
         self.up_down_velocity = 0
         self.yaw_velocity = 0
         self.speed = 10 # initial speed
-        self.S = 20 # speed of the drone when moving
+        self.S = 40 # speed of the drone when moving
 
         # drone controle toggles
         self.interrupt = False
@@ -48,18 +52,14 @@ class droneController:
         # autonomous properties
         # openCV detection box sizes (approximated to use as distance measure)
         # self.box_sizes = [1026, 684, 456, 304, 202, 136, 90]
-        self.box_sizes = [136, 202, 304, 456, 684]
-        self.distance_level = 1
+        self.box_sizes = [680, 460, 300, 200, 140]
+        self.distance_level = 2
         self.frame_size = {'x':960, 'y':720}
         self.frame_center = {'x':self.frame_size['x']/2, 'y':self.frame_size['y']/2}
-        self.heightOffset = 150
+        self.heightOffset = 150 # to let drone hover not directly infront of face
         self.tolerance = 40
         self.target_coordinates = {'x': self.frame_center['x'], 'y': self.frame_center['y'], 'z':400}
-        # restrict detection mechanism to every x seconds to stabalize auto flight and speed up processing
-        self.wait_time = 1.5
-        self.last_detect = time.time()
-        self.locked = False
-        self.lock_on_offset = 300
+        self.face_lost = False
 
         try:
             self.drone.connect()
@@ -155,17 +155,16 @@ class droneController:
                 self.S += 5
         elif key == pygame.K_p: # speed down
                 self.S -= 5
-        elif key == pygame.K_b: # get battery level
-            print("{} percent battery left".format(self.battery()))
         elif key == pygame.K_f: # toggle autonomous mode
             self.autonomous_mode = not self.autonomous_mode
             if self.autonomous_mode:
                 print("Autonomous flight mode active!")
-                self.reset_velocities()
+                self.S = 20
+                self.auto_flight()
             else:
                 print("Autonomous flight mode deactived!")
-                self.reset_velocities()
-                self.auto_flight()
+                self.S = 40
+            self.reset_velocities()
         
         if self.autonomous_mode:
             if key == pygame.K_1:
@@ -178,18 +177,26 @@ class droneController:
                 self.distance_level = 3
             elif key == pygame.K_5:
                 self.distance_level = 4
-            elif key == pygame.K_g:
-                self.lock_on()
         else:
             if key == pygame.K_1:
-                self.S = 20
+                self.S = 10
             elif key == pygame.K_2:
-                self.S = 40
+                self.S = 20
             elif key == pygame.K_3:
-                self.S = 60
+                self.S = 30
             elif key == pygame.K_4:
-                self.S = 80
+                self.S = 40
             elif key == pygame.K_5:
+                self.S = 50
+            elif key == pygame.K_6:
+                self.S = 60
+            elif key == pygame.K_7:
+                self.S = 70
+            elif key == pygame.K_8:
+                self.S = 80
+            elif key == pygame.K_9:
+                self.S = 90
+            elif key == pygame.K_0:
                 self.S = 100
 
         return self.autonomous_mode
@@ -204,29 +211,32 @@ class droneController:
         distance_vector = frame_center_vector - target_position_vector
         # print("Distance Vector ({}, {}, {})".format(distance_vector[0],distance_vector[1],distance_vector[2]))
         
-        # rotate (r-axes)
-        if distance_vector[0] < -self.tolerance:
-            self.yaw_velocity = int(self.S/2)
-        elif distance_vector[0] > self.tolerance:
-            self.yaw_velocity = int(-self.S/2)
+        if self.face_lost:
+            self.face_search_hover()
         else:
-            self.yaw_velocity = 0
-        
-        # up down (y-axes)
-        if distance_vector[1] < -self.tolerance:
-            self.up_down_velocity = -self.S
-        elif distance_vector[1] > self.tolerance:
-            self.up_down_velocity = self.S
-        else:
-            self.up_down_velocity = 0
-        
-        # back forth (z-axes)
-        if distance_vector[2] > 0:
-            self.for_back_velocity = self.S
-        elif distance_vector[2] < 0:
-            self.for_back_velocity = -self.S
-        else:
-            self.for_back_velocity = 0
+            # rotate (r-axes)
+            if distance_vector[0] < -self.tolerance:
+                self.yaw_velocity = int(self.S)
+            elif distance_vector[0] > self.tolerance:
+                self.yaw_velocity = int(-self.S)
+            else:
+                self.yaw_velocity = 0
+            
+            # up down (y-axes)
+            if distance_vector[1] < -self.tolerance:
+                self.up_down_velocity = -self.S
+            elif distance_vector[1] > self.tolerance:
+                self.up_down_velocity = self.S
+            else:
+                self.up_down_velocity = 0
+            
+            # back forth (z-axes)
+            if distance_vector[2] > 0:
+                self.for_back_velocity = self.S
+            elif distance_vector[2] < 0:
+                self.for_back_velocity = -self.S
+            else:
+                self.for_back_velocity = 0
         
                 
     def reset_velocities(self):
@@ -246,9 +256,7 @@ class droneController:
         puts together the frame processing pipeline and returns the frame
         """
         self.frame = self.frame_read.frame
-        if (self.last_detect - time.time()) < self.wait_time:
-            self.detect()
-            self.last_detect = time.time()
+        self.detect()    
         self.clearImage()
         return self.frame
 
@@ -266,49 +274,54 @@ class droneController:
                 cv2.rectangle(self.frame, (x, y), (x+w, y+h),(0, 255, 0), 2)
         """
         if len(faces) != 0:
+            self.face_lost = False
             for (x,y,w,h) in faces:
-                if self.locked:
-                    if (not (self.target_coordinates['x']-self.lock_on_offset <= int(x + w/2)) or 
-                        not (int(x + w/2) <= self.target_coordinates['x']+self.lock_on_offset) or 
-                        not (self.target_coordinates['y']-self.lock_on_offset <= int(y + h/2)) or 
-                        not (int(y + h/2) <= self.target_coordinates['y']+self.lock_on_offset)):
-                        continue
-                    else:
-                        self.target_coordinates['x'] = int(x + w/2)
-                        self.target_coordinates['y'] = int(y + h/2)
-                        self.target_coordinates['z'] = w*2
-                        cv2.rectangle(self.frame, (x, y), (x+w, y+h),(255, 0, 255), 2)
-                        # print("face detected with size: ({} ,{})".format(str(x),str(y)))
-                        # draw targeting cicle if face detected
-                        cv2.circle(self.frame, (int(self.target_coordinates['x']), int(self.target_coordinates['y'])), 10, (0,255,0), 2)    
-                else:
-                    self.target_coordinates['x'] = int(x + w/2)
-                    self.target_coordinates['y'] = int(y + h/2)
-                    self.target_coordinates['z'] = w*2
-                    cv2.rectangle(self.frame, (x, y), (x+w, y+h),(255, 0, 255), 2)
-                    # print("face detected with size: ({} ,{})".format(str(x),str(y)))
-                    # draw targeting cicle if face detected
-                    cv2.circle(self.frame, (int(self.target_coordinates['x']), int(self.target_coordinates['y'])), 10, (0,255,0), 2)
+                self.target_coordinates['x'] = int(x + w/2)
+                self.target_coordinates['y'] = int(y + h/2)
+                self.target_coordinates['z'] = w*2
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h),(255, 0, 255), 2)
+                # print("face detected with size: ({} ,{})".format(str(x),str(y)))
+                # draw targeting cicle if face detected
+                cv2.circle(self.frame, (int(self.target_coordinates['x']), int(self.target_coordinates['y'])), 10, (0,255,0), 2)
         else:
+            if not self.face_lost:
+                self.cycle_start = time.time()
+            self.face_lost = True
             self.target_coordinates['x'] = self.frame_center['x']
             self.target_coordinates['y'] = self.frame_center['y']
             self.target_coordinates['z'] = self.box_sizes[3]
         # draw frame center point
         cv2.circle(self.frame, (int(self.frame_center['x']), int(self.frame_center['y'])), 2, (0,0,255), 2)
+        # in autonomus mode draw a target line from frame_center(view point) to target cicle
+        if self.autonomous_mode:
+            cv2.line(self.frame, (int(self.frame_center['x']), int(self.frame_center['y'])), (int(self.target_coordinates['x']), int(self.target_coordinates['y'])), [255, 0, 0], 1)
+        # show stats
+        cv2.putText(self.frame,"for/back velocity: {}".format(self.for_back_velocity),(5,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        cv2.putText(self.frame,"up/down velocity: {}".format(self.up_down_velocity),(5,50),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        cv2.putText(self.frame,"left/right velocity: {}".format(self.left_right_velocity),(5,70),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        cv2.putText(self.frame,"Speed: {}".format(self.S),(5,90),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        cv2.putText(self.frame,"Auto: {}".format(self.autonomous_mode),(5,110),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        cv2.putText(self.frame,"Auto Dist: {}".format(self.distance_level+1),(5,130),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        
+        try:
+            battery_level = self.drone.get_battery()[:2]
+            cv2.putText(self.frame,"Battery: {}".format(battery_level),(5,150),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
+        except:
+            cv2.putText(self.frame,"Battery: unknown",(5,150),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255),2)
 
-    def lock_on(self):
-        if self.locked:
-            self.locked_coordinates = self.target_coordinates
-            self.locked = not self.locked
+    def face_search_hover(self):
+        # hover up and down to find the face again
+        if self.cycle_start+2 > time.time():
+            self.up_down_velocity = self.S
+            self.for_back_velocity = -self.S
         else:
-            self.locked_coordinates = self.target_coordinates
-            self.locked = not self.locked
+            self.up_down_velocity = -self.S
+            self.for_back_velocity = self.S
+            self.cycle_start = time.time()
+        
 
     def clearImage(self):
         # image clearance
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB) # color converion
         self.frame = np.rot90(self.frame) # rotate image correct
         self.frame = np.flipud(self.frame)
-
-    def battery(self):
-        return self.drone.get_battery()[:2]
